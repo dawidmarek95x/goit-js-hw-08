5d7def7a6f8e635acd714cc943cd4fbbeb103550
dawidmarek95x <dawid.marek95x@gmail.com>
2022-05-24 19:39:51 +0200

    Task 1 completed
    An image gallery has been added that uses the functionality of the SimpleLightbox library.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// SimpleLightbox library import
+import SimpleLightbox from 'simplelightbox';
+
+// Import of SimpleLightbox styles
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Search for ul.gallery element
+const gallery = document.querySelector(".gallery");
+
+// Create and render image tags according to the galleryItems data array and provided gallery item template
+const markupsForGalleryItems = galleryItems
+  .map(({preview, original, description}) => 
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markupsForGalleryItems);
+
+// Implementation of delegation on ul.gallery
+gallery.addEventListener("click", (e) => e.preventDefault());
+
+// Initialization of the SimpleLightbox library and adding displaying captions to images with the alt attribute
+const zoomInWithNav = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// Below is an additional code that changes the aesthetics of the modal
+
+// Change the counter position so that it does not overlap the "go back" text below the modal
+function changeCounterStylesInModal(modal) {
+  const zoomInCounter = modal.domNodes.counter.style;
+  zoomInCounter.top = "40px";
+}
+
+changeCounterStylesInModal(zoomInWithNav);
