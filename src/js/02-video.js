4d9b5edf243303f304169be90e676d0c2f4fa1a2
dawidmarek95x <dawid.marek95x@gmail.com>
2022-05-25 01:03:15 +0200

    Task 2 completed
    A functionality has been added consisting in storing the state of the viewed video after re-visiting the site (until the user clears the cookies).

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+// Import Vimeo Player library
+import Player from '@vimeo/player';
 
+// Import lodash.throttle library
+const throttle = require('lodash.throttle');
+
+// Search for iframe#vimeo-player element
+const video = document.querySelector("#vimeo-player");
+
+// Create an instance of the Player class for the iframe#vimeo-player element
+const player = new Player(video);
+
+// Adding a timeupdate event listener for the 1 second delayed player instance
+player.on("timeupdate", throttle(saveWatchTime, 1000));
+
+// The function saves the current time of playing video to local storage
+function saveWatchTime(evt) {
+  localStorage.setItem("videoplayer-current-time", evt.seconds);
+};
+
+// Setting the time of the video played from local storage
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
